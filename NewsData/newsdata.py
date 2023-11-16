e79d51b78886d67d1f67485e024e63593ab882d4
pkoi5088 <pkoi5088@gmail.com>
2023-11-16 12:02:20 +0900

    [Fix] Fixing a bug that enters news data twice
    - 다중 for문으로 인한 뉴스데이터가 2번씩 들어가는 버그 수정
    issue #38

--- NewsData/newsdata.py
+++ NewsData/newsdata.py
@@ -25,38 +25,36 @@ def newsData():
     # RSS를 통한 뉴스 데이터 수집
     result = RSS()
 
-
     # dictionary 형태의 결과물에서 뉴스 기사별로 조회
     for company in result:
-        for _ in result[company]:
-            companyName = result[company]["companyName"]
-            
-            for topic in result[company]["articles"]:
-                for news in result[company]["articles"][topic]:
-                    title = str(news["title"])
-                    link = news["link"]
-                    context = news["context"]
-                    date = news["date"]
+        companyName = result[company]["companyName"]
+        
+        for topic in result[company]["articles"]:
+            for news in result[company]["articles"][topic]:
+                title = str(news["title"])
+                link = news["link"]
+                context = news["context"]
+                date = news["date"]
+                
+                # TextRank를 통한 키워드 추출 및 데이터베이스 저장
+                try:
+                    keywords = TextRank(context)
+                    # cur.execute("insert into news(companyname, topic, title, link, context, keywords) values(%s, %s, %s, %s, %s, %s);", (companyName, topic, title, link, context, keywords))
+                    cur.execute("insert into news(company_name, topic, title, link, context, post_time) values(%s, %s, %s, %s, %s, %s);", (companyName, topic, title, link, context, date))
+                    conn.commit()
                     
-                    # TextRank를 통한 키워드 추출 및 데이터베이스 저장
-                    try:
-                        keywords = TextRank(context)
-                        # cur.execute("insert into news(companyname, topic, title, link, context, keywords) values(%s, %s, %s, %s, %s, %s);", (companyName, topic, title, link, context, keywords))
-                        cur.execute("insert into news(company_name, topic, title, link, context, post_time) values(%s, %s, %s, %s, %s, %s);", (companyName, topic, title, link, context, date))
+                    cur.execute("select id from news order by id desc limit 1;")
+                    row = cur.fetchone()
+                    
+                    id = row[0]
+                    
+                    for keyword in keywords:
+                        cur.execute("insert into newskeyword(news_id, keyword) values(%s, %s);", (id, keyword))
                         conn.commit()
-                        
-                        cur.execute("select id from news order by id desc limit 1;")
-                        row = cur.fetchone()
-                        
-                        id = row[0]
-                        
-                        for keyword in keywords:
-                            cur.execute("insert into newskeyword(news_id, keyword) values(%s, %s);", (id, keyword))
-                            conn.commit()
-                    except UnicodeDecodeError: # 특수 문자에 대한 예외 처리
-                        continue
-                    except ValueError: # TextRank로 처리하기 힘든 적은 문자수의 기사의 경우 생략
-                        continue
+                except UnicodeDecodeError: # 특수 문자에 대한 예외 처리
+                    continue
+                except ValueError: # TextRank로 처리하기 힘든 적은 문자수의 기사의 경우 생략
+                    continue
                     
                     
 try:
