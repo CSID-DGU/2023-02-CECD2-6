8775acc9c87bf24b4c6120e823b51352ff592f58
Mitchel Song <songminsu2363@gmail.com>
2023-12-03 03:19:04 +0900

    [Feat] Modify constraints in DB
    - Remove NOT NULL contraints in keywords, categories column at Users
      Table
    issue #57

--- GDSDoorcheck/migrations/V004__Change_User_Schema.sql
+++ GDSDoorcheck/migrations/V004__Change_User_Schema.sql
@@ -2,5 +2,5 @@ ALTER TABLE UsersKeywords DROP CONSTRAINT userskeywords_user_id_fkey;
 
 DROP TABLE UsersKeywords;
 
-ALTER TABLE Users ADD topics varchar NOT NULL;
-ALTER TABLE Users ADD keywords varchar NOT NULL;
+ALTER TABLE Users ADD topics varchar;
+ALTER TABLE Users ADD keywords varchar;
