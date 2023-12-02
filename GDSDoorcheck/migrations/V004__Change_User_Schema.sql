481f56cd5f198ee0c40b1cf0be0d333cd8164859
Mitchel Song <songminsu2363@gmail.com>
2023-12-03 03:14:53 +0900

    [Feat] Modify DB structure
    - Delete Table UsersKeywords
    - Add column keywords to Users Table
    - Add column categories to Users Table
    issue #57

---
+++ GDSDoorcheck/migrations/V004__Change_User_Schema.sql
@@ -0,0 +1,6 @@
+ALTER TABLE UsersKeywords DROP CONSTRAINT userskeywords_user_id_fkey;
+
+DROP TABLE UsersKeywords;
+
+ALTER TABLE Users ADD topics varchar NOT NULL;
+ALTER TABLE Users ADD keywords varchar NOT NULL;
