c81db16ea459a252a5c2d966e7682c6f8277c53e
Mitchel Song <songminsu2363@gmail.com>
2023-12-05 01:52:24 +0900

    [Bug] Resolve parameter mapping error
    - By adding @ConstructorProperties, resolve the parameter mapping error
    issue #83

--- backend/src/main/java/multinewssummarizer/backend/user/model/UserSignInRequestDto.java
+++ backend/src/main/java/multinewssummarizer/backend/user/model/UserSignInRequestDto.java
@@ -2,11 +2,18 @@ package multinewssummarizer.backend.user.model;
 
 import lombok.*;
 
+import java.beans.ConstructorProperties;
+
 @Getter
-@AllArgsConstructor
 @Builder
 @ToString
 public class UserSignInRequestDto {
     private String accountId;
     private String password;
+
+    @ConstructorProperties({"accountId", "password"})
+    public UserSignInRequestDto(String accountId, String password) {
+        this.accountId = accountId;
+        this.password = password;
+    }
 }
